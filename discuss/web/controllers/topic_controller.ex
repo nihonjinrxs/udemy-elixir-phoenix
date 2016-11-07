0646f7fa3c854bb010087811e7c368d8021721a4
nihonjinrxs <ryan.b.harvey@ted.com>
2016-11-06 19:32:51 -0600

    Section 13 (partial): Added show topic view and added comments model and database migration.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -16,6 +16,11 @@ defmodule Discuss.TopicController do
     render conn, "index.html", topics: topics
   end
 
+  def show(conn, %{"id" => topic_id}) do
+     topic = Repo.get!(Topic, topic_id)
+     render conn, "show.html", topic: topic
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{})
     render conn, "new.html", changeset: changeset
@@ -71,7 +76,7 @@ defmodule Discuss.TopicController do
   end
 
   def check_topic_owner(conn, _params) do
-    %{params: %{}"id" = topic_id}} = conn
+    %{params: %{"id" => topic_id}} = conn
     if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
       conn
     else
