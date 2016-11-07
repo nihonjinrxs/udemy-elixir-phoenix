190e977e63cdd539c512886281fd48fbccd7d2d6
nihonjinrxs <ryan.b.harvey@ted.com>
2016-11-06 19:05:20 -0600

    Section 12: Authentication and endpoint protection/permissioning complete; topic ownership implemented.

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -6,6 +6,9 @@ defmodule Discuss.TopicController do
   plug Discuss.Plugs.RequireAuth when action in [
     :new, :create, :edit, :update, :delete
   ]
+  plug :check_topic_owner when action in [
+    :edit, :update, :delete
+  ]
 
   def index(conn, _params) do
     IO.inspect conn.assigns
@@ -19,7 +22,10 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+    changeset = conn.assigns.user
+      |> build_assoc(:topics)
+      |> Topic.changeset(topic)
+
     case Repo.insert(changeset) do
       {:ok, _topic} ->
         conn
@@ -62,5 +68,17 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{}"id" = topic_id}} = conn
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You can't perform this action with this topic.")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
   end
 end
